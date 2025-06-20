e4494cf3b08f1ad78c2521d86c79cbaed5d4775b
puranchandra <157471021+puranchandra@users.noreply.github.com>
2025-06-20 23:47:03 +0530

    Add contact link to header and create contact page

--- components/Header.vue
+++ components/Header.vue
@@ -3,6 +3,7 @@
         <NuxtLink to="/" style="margin-right: 20px;">Home</NuxtLink>
         <NuxtLink to="/about" style="margin-right: 20px;">About</NuxtLink>
         <NuxtLink to="/services" style="margin-right: 20px;">Services</NuxtLink>
+        <NuxtLink to="/contact" style="margin-right: 20px;">Contact</NuxtLink>
 
     </nav>
 </template>
