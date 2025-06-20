e84d850faca28af3abb8032976ab0957b5320840
puranchandra <157471021+puranchandra@users.noreply.github.com>
2025-06-20 23:42:49 +0530

    Add services link to header and create services page

--- components/Header.vue
+++ components/Header.vue
@@ -2,6 +2,7 @@
     <nav style="display: flex; justify-content: start; align-items: center;position: sticky; top: 0;">
         <NuxtLink to="/" style="margin-right: 20px;">Home</NuxtLink>
         <NuxtLink to="/about" style="margin-right: 20px;">About</NuxtLink>
+        <NuxtLink to="/services" style="margin-right: 20px;">Services</NuxtLink>
 
     </nav>
 </template>
---
+++ pages/services.vue
@@ -0,0 +1,5 @@
+<template>
+    <p>Services Page</p>
+    <p>Welcome to our services page!</p>
+    <p>Here you can find a list of services we offer, including web development, mobile app development, and more.</p>
+</template>
